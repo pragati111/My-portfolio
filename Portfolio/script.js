dc4d7ba0226efab39c2e316aa4e9b5dd30cd11c5
Pragati Agarwal <pragatinov@gmail.com>
2025-05-13 16:45:00 +0530

    This is the file added for that multiple names you can give yourself removing one and updating with a new one animation.

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    String: ["Flutter-Developer", "MERN Developer", "ML Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+})
