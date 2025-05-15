6f1864fb78acae18d26c7f98fd97d488dc8440d5
Pragati Agarwal <pragatinov@gmail.com>
2025-05-15 16:10:05 +0530

    We have made changes to index.html and script.js such that now animation of multiple positions displaying (erasing one and typing another) works properly. Changes : in script.js changed String to strings

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    String: ["Flutter-Developer", "MERN Developer", "ML Engineer"],
+    strings: ["Flutter-Developer", "ML Engineer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
